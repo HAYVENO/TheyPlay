20db2e1a98d6ff756b9f9bad28f7887664b147d4
Abdulhafeez Yusuf <haythepen@gmail.com>
2023-05-03 12:13:23 +0100

    add metatags  :mag:

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,37 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="icon" href="/favicon.ico" />
+				<meta name="title" content="TheyPlay: Explore New Music with Your Playgroup" />
 				<meta
 					name="description"
-					content="Discover the ultimate music app for playgroups with TheyPlay. Empowering community members with the ability to choose and control what they listen to, it is ideal for facilitating fun, engaging, and personalized music experiences. "
+					content="Empowering community members to share and discover the music they love for a fun, engaging, and personalized music experience."
 				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://they-play.vercel.app/" />
+				<meta property="og:title" content="TheyPlay: Explore New Music with Your Playgroup" />
+				<meta
+					property="og:description"
+					content="Empowering community members to share and discover the music they love for a fun, engaging, and personalized music experience."
+				/>
+				<meta
+					property="og:image"
+					content="https://res.cloudinary.com/detye5zx5/image/upload/v1682695340/TheyPlay-teaser-home_sc9wtl.png"
+				/>
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://they-play.vercel.app/" />
+				<meta
+					property="twitter:title"
+					content="TheyPlay: Explore New Music with Your Playgroup"
+				/>
+				<meta
+					property="twitter:description"
+					content="Empowering community members to share and discover the music they love for a fun, engaging, and personalized music experience."
+				/>
+				<meta
+					property="twitter:image"
+					content="https://res.cloudinary.com/detye5zx5/image/upload/v1682695340/TheyPlay-teaser-home_sc9wtl.png"
+				/>
+				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<body>
 				<Main />
